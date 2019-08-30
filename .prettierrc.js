ad77bf125a616c631d29fbb5ca5daa37efe75983
zhangchao <544262408@qq.com>
2019-08-30 18:31:01 +0800

    eslint 配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // trailingComma: 'es5',
+  // tabWidth: 2,
+  semi: true, // 分号
+  singleQuote: true, // 单引号
+  printWidth: 120,
+  parser: 'vue',
+  htmlWhitespaceSensitivity: 'ignore'
+};
