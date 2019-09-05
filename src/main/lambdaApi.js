209535b54c5c29e106923c1ab43460d16daae319
zhangchao <544262408@qq.com>
2019-09-05 20:25:26 +0800

    eslint

--- src/main/lambdaApi.js
+++ src/main/lambdaApi.js
@@ -1,17 +1,15 @@
+import Rpcapijs from '@jswebfans/rpcapijs';
 var { DAEMON_CONFIG } = require('../configmain.js');
-import CosmosAPI from "@jswebfans/cosmos-api"
-var  API;
-export default function(){
-    return ()=>{
-     API= new CosmosAPI(DAEMON_CONFIG.LambdaNetwork(),'lambda-hub-test')
+var API;
+export default function() {
+  return () => {
+    API = new Rpcapijs(DAEMON_CONFIG.LambdaNetwork(), 'lambda-hub-test');
     return API;
-
-    }
-    
+  };
 }
 
-export function changeCosmosAPI(){
-    API= new CosmosAPI(DAEMON_CONFIG.LambdaNetwork(),'lambda-hub-test')
-    return true;
+export function changeCosmosAPI() {
+  API = new Rpcapijs(DAEMON_CONFIG.LambdaNetwork(), 'lambda-hub-test');
+  return true;
 }
 
