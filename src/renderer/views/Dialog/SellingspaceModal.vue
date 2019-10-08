794f5c9c8a08be7725393a5917bfc3a79cfd1f25
zhangchao <544262408@qq.com>
2019-10-08 20:47:58 +0800

    add file and route

---
+++ src/renderer/views/Dialog/SellingspaceModal.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <Modal
+      loading
+      v-model="withdrawalModal"
+      title="出售空间"
+      :styles="{top: '200px'}"
+      @on-cancel="sendcancel"
+    >
+      <p>
+        <Input readonly v-model="DistributionReward">
+          <span slot="prepend">空间</span>
+          <span slot="append">TBB</span>
+        </Input>
+      </p>
+      <div slot="footer">
+        <Button type="primary" @click="prewithdrawalLAMB">{{$t('home.Modal1.Submit')}}</Button>
+      </div>
+    </Modal>
+    <Modal v-model="confirmModal" :styles="{top: '200px'}">
+      <div class="modal-header" slot="header">
+        <h2>{{$t('Dialog.withdrawalModal.title')}}</h2>
+        <Row class-name="item">
+          <Col span="4" class-name="key">{{$t('home.Modal1.From')}}:</Col>
+          <Col span="20" class-name="value">{{address}}</Col>
+        </Row>
+        <Row class-name="item">
+          <Col span="4" class-name="key">{{$t('home.Modal1.Amount')}}:</Col>
+          <Col span="20" class-name="value">{{DistributionReward}} LAMB</Col>
+        </Row>
+        <Row class-name="item">
+            <Input  v-model="gaseFee" >
+                              <span slot="prepend">{{$t('Dialog.com.gasfee')}}</span>
+                                <span slot="append">LAMB</span>
+                              </Input>
+          </Row>
+      </div>
+      <!-- <p>
+          <Input v-model="walletPassword" type="password"></Input>
+      </p>-->
+      <div slot="footer">
+        <Button type="primary" @click="confirm">{{$t('home.Modal1.Confirm')}}</Button>
+      </div>
+    </Modal>
+  </div>
+</template>
+<script>
+import eventhub from '../../common/js/event.js';
+const { ipcRenderer: ipc } = require('electron-better-ipc');
+export default {
+  data() {
+    return {
+      withdrawalModal: false,
+      confirmModal: false,
+      gaseFee: 0
+    };
+  },
+  methods: {
+    open() {
+      console.log('- -');
+      this.$data.withdrawalModal = true;
+      this.$data.confirmModal = false;
+    },
+    prewithdrawalLAMB() {
+      console.log('- -');
+      this.LAMBvalue = this.DistributionReward;
+      let value = this.toBigNumStr(this.LAMBvalue);
+
+      // if (value <= 0 || value > this.$data.DistributionReward ) {
+      //   // need to alert
+      //   this.$Notice.warning({
+      //     title: this.$t('home.action.check_balance_amount_transfer')
+      //   });
+      //   return;
+      // }
+
+      if (isNaN(value)) {
+        this.$Notice.warning({
+          title: this.$t('home.action.Check_the_amount')
+        });
+        return;
+      }
+      this.$data.withdrawalModal = false;
+      this.transfer(value, 'withdrawal');
+    },
+    async transfer(amount, txType) {
+      let to = this.Tovalue;
+      // let amount = this.LAMBvalue;
+      let gas = 1;
+      // amount = amount * 10000;
+      this.$data.transactiondata = null;
+      try {
+        let res = await ipc.callMain(txType, {
+          to,
+          amount,
+          gas
+        });
+        // console.log(res);
+        if (res.state) {
+          console.log(res.data);
+          let gasres = await ipc.callMain('Simulate', { transactiondata: res.data });
+          if (gasres.state) {
+            this.$data.gaseFee = gasres.data;
+            this.$data.transactiondata = res.data;
+            this.sendcancel();
+            this.confirmModal = true;
+          }
+        }
+      } catch (ex) {
+        this.$Notice.warning({
+          title: 'error',
+          desc: ex.errormsg
+        });
+        console.log(ex);
+      }
+    },
+    sendcancel() {
+      this.withdrawalModal = false;
+      // this.confirmModal=true;
+    },
+    confirm() {
+      var comparedNum = this.bigNum(this.toBigNumStr(this.$data.gaseFee)).comparedTo(this.$store.getters.balanceLamb);
+      if (comparedNum == 1 || comparedNum == null || comparedNum == 0) {
+        this.$Notice.warning({
+          title: 'error',
+          desc: this.$t('Dialog.com.Lesscommission')
+        });
+        return;
+      }
+      this.confirmModal = false;
+      console.log(this.$data.transactiondata);
+      eventhub.$emit('TxConfirm', this.$data.transactiondata, this.toBigNumStr(this.$data.gaseFee));
+    }
+  },
+  computed: {
+    DistributionReward() {
+      return this.bigNumType(this.$store.getters.getDistributionReward);
+    },
+    address: function() {
+      return this.$store.getters.getaddress;
+    },
+    balance: function() {
+      return this.$store.getters.getblance;
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.modal-header {
+  .item {
+    margin-top: 20px;
+    font-size: 14px;
+  }
+}
+</style>
+
