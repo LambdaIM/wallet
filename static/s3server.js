f38e6b56c314127272f5a053e778327d7d495276
zhangchao <544262408@qq.com>
2019-10-28 20:16:39 +0800

    s3 集成

--- static/s3server.js
+++ static/s3server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
 const hdkeyjs = require('@jswebfans/hdkeyjs');
+var md5 = require('md5');
 console.log('==============s3server')
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -51,7 +52,7 @@ app.use(bodyParser.json())
 
 // account(app);
 app.get('/', function (req, res) {
-  res.send('Hello World elect')
+  res.send('Hello World Lamb storage')
 })
 var defaultwallet,password;
 app.post('/authorization', function (req, res) {
@@ -80,7 +81,7 @@ app.get('/isauthorization', function (req, res) {
   if(defaultwallet==undefined||password==undefined){
     res.send({ok:false,error:'need defaultwallet or password'})
   }else{
-    res.send({ok:true})
+    res.send({ok:true,PubKey:defaultwallet.publicKey })
   }
 })
 
@@ -89,7 +90,18 @@ app.post('/s3signature', function (req, res) {
     res.send({ok:false,error:'need defaultwallet or password'})
   }else{
     content=req.body.content ;
-  var contentBuffer = Buffer.from(content);
+    var filejson={};
+  try {
+    filejson=JSON.parse(content);
+  } catch (error) {
+    res.send({ok:false,error:'need json content'})
+  }
+  if(checkfileJson(filejson)==false){
+    res.send({ok:false,error:'need json fileinfo content'})
+  }
+  var Digestcontent= md5(content);
+
+  var contentBuffer = Buffer.from(Digestcontent);
 
   var privatekey = hdkeyjs.keyStore.checkJson(defaultwallet, password);
 
@@ -97,10 +109,40 @@ app.post('/s3signature', function (req, res) {
     contentBuffer,
     privatekey
   );
-    res.send({ok:true,data:sindata})
+    res.send({ok:true,data:{
+      signature:sindata,
+      digest:Digestcontent
+    }})
   }
 })
 
+function checkfileJson(filejson){
+  /*
+  {
+	"order_id": "B1FD3CBA514E33167E11D1673EA0484DB5E5858B",
+	"file_status": 1,
+	"file_path": "a/b/c",
+	"blockhash": ["fdsafwfefaw21212", "fafew1212"]
+}
+  
+  */
+var result;
+ if(filejson.order_id!=undefined&&typeof filejson.order_id=='string'){
+  result=true;
+ }
+
+ if(filejson.file_status!=undefined&&typeof filejson.file_status=='number' ){
+  result=true;
+ }
+
+ if(filejson.file_path!=undefined&&typeof filejson.file_path=='string'){
+  result=true;
+ }
+ if(filejson.blockhash!=undefined&&filejson.blockhash instanceof Array){
+  result=true;
+ }
+  return result; 
+}
 
 
 
