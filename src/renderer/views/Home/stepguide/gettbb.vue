3f15b0f0433facb78059789ced34d430e0ab2857
zhangchao <544262408@qq.com>
2020-02-21 10:35:07 +0800

    1step 多语言

--- src/renderer/views/Home/stepguide/gettbb.vue
+++ src/renderer/views/Home/stepguide/gettbb.vue
@@ -2,29 +2,28 @@
 <div>
       <Row>
         <Col span="12">
-        <h4>1通过lamb 兑换TBB</h4>
+        <h4>{{$t('guidepage.step1.title1')}}</h4>
         <br/><br/>
-        <Button  @click="openAssert" type="primary" >兑换TBB</Button>
+        <Button  @click="openAssert" type="primary" >{{$t('guidepage.step1.btnConvert')}}</Button>
         <br/><br/>
 
 
         </Col>
         <Col span="12">
-        <h4>2通过交易所 购买TBB</h4>
+        <h4>{{$t('guidepage.step1.title2')}}</h4>
         <br/><br/>
         <span>
-            可以到康波交易所购买TBB<br/>
-            康波交易所 <a @click="openweb('https://kangbo.io/')">https://kangbo.io/ </a>
+            {{$t('guidepage.step1.title2brief')}}<a @click="openweb('https://kangbo.io/')">https://kangbo.io/ </a>
         </span>
 
         </Col>
     </Row>
     <Row>
       <Col span="12">
-        <h4>3 测试网进行挖矿需要先获取测试币</h4>
+        <h4>{{$t('guidepage.step1.title3')}}</h4>
         <br/><br/>
         <span>
-          获取测试币地址 <a @click="openweb('http://faucet.lambda.im/')">http://faucet.lambda.im/</a>
+          {{$t('guidepage.step1.title3brief')}} <a @click="openweb('http://faucet.lambda.im/')">http://faucet.lambda.im/</a>
         </span>
 
         </Col>
