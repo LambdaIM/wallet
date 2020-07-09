b7aedbd149398736db0fce3660aa8ee366c69d45
zhangchao <544262408@qq.com>
2020-07-09 13:26:56 +0800

    匹配订单

--- src/renderer/views/marketasset/redeemlist.vue
+++ src/renderer/views/marketasset/redeemlist.vue
@@ -3,13 +3,74 @@
     <div class="customer-container">
 
       <div class="tableContainer">
-        <h1> 赎回列表  </h1>
+                <Table  :columns="redeemcolumns" :data="redeemdata">
+                  <template slot-scope="{ row, index }" slot="completionTime">
+                    {{row.completionTime|blockFormatDate}}
+                  </template>
+                  <template slot-scope="{ row, index }" slot="cost">
+                    {{row.cost|BlanceValue}}
+                  </template>
+
+                  <template slot-scope="{ row, index }" slot="assetName">
+                    {{denomFormart(row.assetName)}}
+                  </template>
+                </Table>
         </div>
     </div>
 </div>
 </template>
 <script>
+import _ from 'underscore';
+
+import eventhub from '../../common/js/event.js';
+import { DAEMON_CONFIG } from '../../../config.js';
+
+
+const { shell } = require('electron');
+const { ipcRenderer: ipc } = require('electron-better-ipc');
+
+
+
 export default {
+  data() {
+    return {
+      redeemcolumns: [
+        {
+          title: this.$t('assetpage.Redemptionlist.AssetName'),
+          key: 'assetName',
+          slot: 'assetName'
+        },
+        {
+          title: this.$t('assetpage.Redemptionlist.Redemptionamount'),
+          key: 'cost',
+          slot: 'cost'
+        },
+        {
+          title: this.$t('assetpage.Redemptionlist.Completetime'),
+          key: 'completionTime',
+          slot: 'completionTime'
+        }
+      ],
+      redeemdata: []
+    };
+  },
+  mounted() {
+    this.getredeemlist();
+  },
+  methods: {
+    async  getredeemlist() {
+      // assetAll
+      console.log('Authorizedredeemlist');
+      try {
+        let res = await ipc.callMain('Authorizedredeemlist', {});
+        if (res.state && res.data.data.error == undefined) {
+          this.$data.redeemdata = res.data.data || [];
+        }
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+  }
 
 };
 </script>
