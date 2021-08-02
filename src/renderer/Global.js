67775345a4a04194f37db24c74104064daf53264
zhangchao <544262408@qq.com>
2021-08-02 10:30:26 +0800

    格式化

--- src/renderer/Global.js
+++ src/renderer/Global.js
@@ -106,7 +106,7 @@ export default {
       } else {
         bigvalue = new BigNumber(bigvalue);
         bigvalue = bigvalue.div(1e6).toFixed(6);
-        return `${new BigNumber(bigvalue).toFormat()}`;
+        return `${new BigNumber(bigvalue).toString()}`;
       }
     };
     Vue.prototype.tomLAMB = function (lambname) {
